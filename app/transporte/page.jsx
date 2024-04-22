e356cdc4a638e26001ee99f6aa71210776f31732
hermani <hermani457@gmail.com>
2024-04-22 13:35:54 -0400

    update

--- app/transporte/page.jsx
+++ app/transporte/page.jsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer.jsx";
 import FoodCard from "@/components/FoodCard";
 import { transporte } from "../../utils/index.js";
 import fordTransfer from "public/assets/transporte/ford-transit.avif";
+import willkatransporte from "public/assets/transporte/willkatransporte.png";
 import { motion, useTransform, useScroll } from "framer-motion";
 import { useRef } from "react";
 
@@ -22,12 +23,12 @@ const page = () => {
           Transporte
         </h1>
         <div ref={ref}>
-          <header className="relative h-dvh h-screen flex justify-center text-white font-montserrat">
+          <header className="relative h-screen flex justify-center text-white font-montserrat">
             <div className="absolute inset-0">
               <motion.div style={{ overflow: "hidden" }}>
                 <motion.div style={{ scale }} className="h-[100svh] h-[100vh]">
                   <Image
-                    src={fordTransfer}
+                    src={willkatransporte}
                     alt="Your image"
                     // sizes="100vw"
                     // fill
@@ -38,22 +39,20 @@ const page = () => {
                 </motion.div>
               </motion.div>
             </div>
-            <div className="relative flex items-center flex-col justify-center">
-              {/* <h2 className="text-4xl sm:text-6xl lg:text-8xl">Titulo</h2> */}
-              <motion.p
-                style={{ y: scale }}
-                className="mx-10 md:mx-36 xl:mx-96 text-md xl:text-xl font-bold bg-white text-black p-5 opacity-50 rounded-xl"
-              >
-                Descubre la excelencia en transporte con Servicios Willka.
-                Nuestros vehículos de primera clase combinados con un enfoque
-                total en seguridad te garantizan viajes cómodos y sin
-                preocupaciones. Ya sea para traslados corporativos, turísticos o
-                personales, confía en nuestra flota y en nuestro compromiso con
-                tu bienestar. ¡Reserva tu viaje hoy y experimenta un transporte
-                de calidad y confianza!
-              </motion.p>
-            </div>
+            {/* <div className="relative flex items-center flex-col justify-center">
+             
+            </div> */}
           </header>
+        </div>
+        <div className="max-w-screen-lg mx-auto text-center">
+          <p className="text-md xl:text-xl font-bold text-white p-5 rounded-xl">
+            Descubre la excelencia en transporte con Servicios Willka. Nuestros
+            vehículos de primera clase combinados con un enfoque total en
+            seguridad te garantizan viajes cómodos y sin preocupaciones. Ya sea
+            para traslados corporativos, turísticos o personales, confía en
+            nuestra flota y en nuestro compromiso con tu bienestar. ¡Reserva tu
+            viaje hoy y experimenta un transporte de calidad y confianza!
+          </p>
         </div>
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="text-3xl sm:text-4xl lg:text-6xl text-center font-museomoderno font-bold tracking-tight text-white mb-12 sm:mb-20">
